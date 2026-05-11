5c6c3c3c17b5f13762678a705495d94983ae4a43
Sthoaaa <thoa.jhoel@outlook.com>
2026-05-10 19:10:06 -0500

    Corregido

--- Hi.java
+++ Hi.java
@@ -1,8 +1,6 @@
 public class Hi {
     public static void main(String[] args) {
-        System.out.println("Hola, Hermano!")
-        System.out.println("¿Cómo estás?");
+        System.out.println("Hola, Hermano");
+        System.out.println("¿Cómo estás hoy?");
     }
-
-
 }
